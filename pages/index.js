e407cb0305ff75a1589b41fc63a7a2d4e738c263
Yotushka <serlight@ukr.net>
2023-05-05 02:03:49 +0300

    deleted some files

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import DefaultLayout from '@/features/Layouts/DefaultLayout'
 import HeroBanner from '@/features/Home/components/HeroBanner'
-import FeaturedProperties from '@/features/Home/components/FeaturedProperties/FeaturedProperties';
 import MeetTheTeam from '@/features/Home/components/MeetTheTeam';
 import Partners from '@/features/Home/components/Partners';
 import Testimonials from '@/features/Home/components/Testimonials';
@@ -11,8 +10,6 @@ export default function Home({ featuredProperties }) {
   return (
     <DefaultLayout>
       <HeroBanner />
-      <FeaturedProperties
-        featuredProperties={featuredProperties} />
       <MeetTheTeam />
       <Partners />
       <Testimonials />
